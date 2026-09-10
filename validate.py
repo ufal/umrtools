747d02e2e0e2f1b41f1503dc39f474191cb6a1a7
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-09-10 19:43:53 +0200

    :ARG2 of have-quant-91 has numeric value.

--- validate.py
+++ validate.py
@@ -1394,7 +1394,8 @@ def validate_relations(sentence, node_dict, args):
                         type = 'attribute'
                         values = ['+', '-']
                     # :ARG1 of rate-entity-91 has numeric value
-                    if r['relation'] == ':ARG1' and node['concept'] == 'rate-entity-91':
+                    # :ARG2 of have-quant-91 has numeric value
+                    if r['relation'] == ':ARG1' and node['concept'] == 'rate-entity-91' or r['relation'] == ':ARG2' and node['concept'] == 'have-quant-91':
                         type = 'attribute'
                     if type != 'attribute':
                         if r['type'] != 'node':
