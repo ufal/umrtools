4a0905fb201f1aa853dfaff01f5e7f353a360597
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-09-14 17:00:04 +0200

    Added Czech-specific relations.

--- validate.py
+++ validate.py
@@ -1330,12 +1330,16 @@ known_relations = {
     ':according-to': {'type': 'modifier', 'repeat': False}, # child node is the source of the information
     ':clausal-marker': {'type': 'modifier', 'repeat': True},
     ':comparison': {'type': 'modifier', 'repeat': False},
+    ':contrast-91': {'type': 'modifier', 'repeat': True},
     ':effect': {'type': 'modifier', 'repeat': True},
     ':interjection': {'type': 'modifier', 'repeat': True},
     ':parenthesis': {'type': 'modifier', 'repeat': True},
     ':part-of-phraseme': {'type': 'modifier', 'repeat': False},
     ':predicative-noun': {'type': 'modifier', 'repeat': False},
     ':regard': {'type': 'modifier', 'repeat': True},
+    ':ARGm-EXT': {'type': 'modifier', 'repeat': True},
+    ':ARGm-LOC': {'type': 'modifier', 'repeat': True},
+    ':ARGm-MNR': {'type': 'modifier', 'repeat': True},
     # The following are also not in the guidelines but are so frequent in the
     # English data converted from AMR that it is hard to see other errors if
     # we keep reporting these.
